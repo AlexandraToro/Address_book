bae2166bf88279deaec7855c391813335436ddb9
Alexandra <sashatoropova@gmail.com>
2022-08-06 20:12:30 +0700

    corrected search

--- Search.py
+++ Search.py
@@ -1,5 +1,16 @@
+from options import database
+
+
 def search():
-    # здесь нужно запросить данные для поиска и вывести список
-    # данные параметров поиска также можно использовать в функции удаления
-    print("Модуль не готов!")
-    return
+    name = str(input('Введите Фамилию или Имя для поиска: '))
+    flag = 0
+    with open(database, 'r', encoding='utf-8') as data:
+        lines = data.readlines()
+        for line in lines:
+            if name in line:
+                print(line)
+                flag = 1
+            else:
+                continue
+    if flag == 0:
+        print("По заданным параметрам данных не найдено.\n")
